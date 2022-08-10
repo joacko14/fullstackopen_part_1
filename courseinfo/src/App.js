29133a10b7bbb8445ff6b73d01ea0b7e7d76c017
Joaquin Vazquez <joacko14@gmail.com>
2022-08-09 23:11:11 -0500

    1.1: course information, step1

---
+++ courseinfo/src/App.js
@@ -0,0 +1,38 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <p>
+          {part.title} {part.exercises}
+        </p>
+      ))}
+    </>
+  );
+};
+
+const Total = ({ total }) => <h3>Number of exercises {total}</h3>;
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { title: "Fundamentals of React", exercises: 10 },
+    { title: "Using props to pass data", exercises: 7 },
+    { title: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total
+        total={parts
+          .map((part) => part.exercises)
+          .reduce((acc, cur) => acc + cur)}
+      />
+    </div>
+  );
+};
+
+export default App;
