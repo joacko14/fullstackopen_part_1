f47508369a80aac39cf05cc1198fc62c41651987
Joaquín Vázquez <joacko14@churchofjesuschrist.com>
2025-08-14 11:42:29 -0600

    part1, exercise 1.6 - Implement feedback buttons and statistics display in App component; add global styles

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -4,7 +4,35 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  return <></>;
+
+  const handleGood = (event) => {
+    event.preventDefault();
+    setGood((prev) => prev + 1);
+    console.log("good button clicked");
+  };
+  const handleNeutral = (event) => {
+    event.preventDefault();
+    setNeutral((prev) => prev + 1);
+    console.log("Neutral button clicked");
+  };
+  const handleBad = (event) => {
+    event.preventDefault();
+    setBad((prev) => prev + 1);
+    console.log("Bad button clicked");
+  };
+
+  return (
+    <>
+      <h3>give feedback</h3>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h3>statistics</h3>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
 }
 
 export default App;
