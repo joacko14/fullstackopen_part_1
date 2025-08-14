502715295e9fd7195448041822214c7e3bff00fb
Joaquín Vázquez <joacko14@churchofjesuschrist.com>
2025-08-14 12:07:21 -0600

    part1, exercise 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [statistics, setStatistics] = useState({
+    all: 0,
+    average: 0,
+    positive: 0,
+  });
 
   const handleGood = (event) => {
     event.preventDefault();
@@ -21,16 +26,40 @@ function App() {
     console.log("Bad button clicked");
   };
 
+  useEffect(() => {
+    setStatistics((prev) => ({
+      ...prev,
+      all: good + neutral + bad,
+      average: (good - bad) / (good + neutral + bad),
+      positive: good / (good + neutral + bad),
+    }));
+  }, [good, neutral, bad]);
+
   return (
     <>
-      <h3>give feedback</h3>
+      <h3>Give feedback </h3>
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <h3>statistics</h3>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <h3>Statistics</h3>
+      <p>
+        good <b>{good}</b>
+      </p>
+      <p>
+        neutral <b>{neutral}</b>
+      </p>
+      <p>
+        bad <b>{bad}</b>
+      </p>
+      <p>
+        all <b>{statistics.all}</b>
+      </p>
+      <p>
+        average <b>{isNaN(statistics.average) ? "-" : statistics.average}</b>
+      </p>
+      <p>
+        positive <b>{isNaN(statistics.positive) ? "-" : statistics.positive}</b>
+      </p>
     </>
   );
 }
